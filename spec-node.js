ae5591858deee143f7ae0264e3ec4d82667a489e
Dem Pilafian <dem@centerkey.com>
2018-09-11 14:40:05 -0700

    Test cases for non-json documents

--- spec-node.js
+++ spec-node.js
@@ -251,6 +251,76 @@ describe('HTTP error returned by httpbin.org', () => {
    });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('The "bodyText" field of the object returned from requesting', () => {
+
+   function getFirstLine(string) { return string.split('\n', 1)[0]; }
+
+   it('an HTML web page is a string that begins with "<!DOCTYPE html>"', (done) => {
+      const url = 'https://httpbin.org/html';
+      function handleData(data) {
+         const actual =   {
+            ok:          data.ok,
+            status:      [data.status, data.statusText],
+            contentType: data.contentType,
+            firstLine:   getFirstLine(data.bodyText)
+            };
+         const expected = {
+            ok:          true,
+            status:      [200, 'OK'],
+            contentType: 'text/html; charset=utf-8',
+            firstLine:   '<!DOCTYPE html>'
+            };
+         assert.deepEqual(actual, expected);
+         done();
+         }
+      fetchJson.get(url).then(handleData);
+      });
+
+   it('an XML document is a string that begins with "<!DOCTYPE xml>"', (done) => {
+      const url = 'https://httpbin.org/xml';
+      function handleData(data) {
+         const actual =   {
+            ok:          data.ok,
+            status:      [data.status, data.statusText],
+            contentType: data.contentType,
+            firstLine:   getFirstLine(data.bodyText)
+            };
+         const expected = {
+            ok:          true,
+            status:      [200, 'OK'],
+            contentType: 'application/xml',
+            firstLine:   '<?xml version=\'1.0\' encoding=\'us-ascii\'?>'
+            };
+         assert.deepEqual(actual, expected);
+         done();
+         }
+      fetchJson.get(url).then(handleData);
+      });
+
+   it('a "robots.txt" text file is a string that begins with "User-agent: *"', (done) => {
+      const url = 'https://httpbin.org/robots.txt';
+      function handleData(data) {
+         const actual =   {
+            ok:          data.ok,
+            status:      [data.status, data.statusText],
+            contentType: data.contentType,
+            firstLine:   getFirstLine(data.bodyText)
+            };
+         const expected = {
+            ok:          true,
+            status:      [200, 'OK'],
+            contentType: 'text/plain',
+            firstLine:   'User-agent: *'
+            };
+         assert.deepEqual(actual, expected);
+         done();
+         }
+      fetchJson.get(url).then(handleData);
+      });
+
+   });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('Function fetchJson.enableLogger()', () => {
 
    it('sets the logger to the function passed in', () => {
