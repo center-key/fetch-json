2bf83fd8806ee4bc3280f073388602d372e6deca
Dem Pilafian <dem@centerkey.com>
2018-12-04 17:11:15 -0800

    Release v2.2.0

--- dist/fetch-json.js
+++ dist/fetch-json.js
@@ -1,25 +1,23 @@
-//! fetch-json v2.1.3
-//! A wrapper around Fetch just for JSON
-//! https://github.com/center-key/fetch-json -- MIT License
+//! fetch-json v2.2.0 -- github.com/center-key/fetch-json -- MIT License
 
 const fetch = typeof window === 'object' && window.fetch || require('node-fetch');
 
 const fetchJson = {
-   version: '2.1.3',
-   request: function(method, url, data, options) {
+   version: '2.2.0',
+   request: (method, url, data, options) => {
       const settings = { method: method.toUpperCase(), credentials: 'same-origin' };
       options = Object.assign(settings, options);
       const jsonHeaders = { 'Content-Type': 'application/json', 'Accept': 'application/json' };
       options.headers = Object.assign(jsonHeaders, options.headers);
-      function toPair(key) { return key + '=' + encodeURIComponent(data[key]); }
+      const toPair = (key) => key + '=' + encodeURIComponent(data[key]);
       if (options.method === 'GET' && data)
          url = url + (url.includes('?') ? '&' : '?') + Object.keys(data).map(toPair).join('&');
       else if (options.method !== 'GET' && data)
          options.body = JSON.stringify(data);
-      function toJson(response) {
+      const toJson = (response) => {
          const contentType = response.headers.get('content-type');
          const isJson = /json|javascript/.test(contentType);  //match "application/json" or "text/javascript"
-         function textToObj(httpBody) {
+         const textToObj = (httpBody) => {
             if (fetchJson.logger)
                fetchJson.logger(new Date().toISOString(), options.method, response.url,
                   response.ok, response.status, response.statusText, contentType);
@@ -27,30 +25,20 @@ const fetchJson = {
             response.contentType = contentType;
             response.bodyText =    httpBody;
             return response;
-            }
+            };
          return isJson ? response.json() : response.text().then(textToObj);
-         }
+         };
       if (fetchJson.logger)
          fetchJson.logger(new Date().toISOString(), options.method, url);
       return fetch(url, options).then(toJson);
       },
-   get: function(url, params, options) {
-      return fetchJson.request('GET', url, params, options);
-      },
-   post: function(url, resource, options) {
-      return fetchJson.request('POST', url, resource, options);
-      },
-   put: function(url, resource, options) {
-      return fetchJson.request('PUT', url, resource, options);
-      },
-   patch: function(url, resource, options) {
-      return fetchJson.request('PATCH', url, resource, options);
-      },
-   delete: function(url, resource, options) {
-      return fetchJson.request('DELETE', url, resource, options);
-      },
+   get:    (url, params, options) =>   fetchJson.request('GET',    url, params,   options),
+   post:   (url, resource, options) => fetchJson.request('POST',   url, resource, options),
+   put:    (url, resource, options) => fetchJson.request('PUT',    url, resource, options),
+   patch:  (url, resource, options) => fetchJson.request('PATCH',  url, resource, options),
+   delete: (url, resource, options) => fetchJson.request('DELETE', url, resource, options),
    logger: null,
-   enableLogger: function(booleanOrFn) {
+   enableLogger: (booleanOrFn) => {
       const isFn = typeof booleanOrFn === 'function';
       fetchJson.logger = isFn ? booleanOrFn : booleanOrFn === false ? null : console.log;
       return fetchJson.logger;
