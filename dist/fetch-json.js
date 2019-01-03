9558aa005467fd5c9d2a8e7f759b9a433d371c87
Dem Pilafian <dem@centerkey.com>
2019-01-02 17:27:04 -0800

    Release v2.2.4

--- dist/fetch-json.js
+++ dist/fetch-json.js
@@ -1,45 +1,49 @@
-//! fetch-json v2.2.3 ~ github.com/center-key/fetch-json ~ MIT License
+//! fetch-json v2.2.4 ~ github.com/center-key/fetch-json ~ MIT License
 
 const fetch = typeof window === 'object' && window.fetch || require('node-fetch');
 
 const fetchJson = {
-   version: '2.2.3',
+   version: '2.2.4',
    request: (method, url, data, options) => {
       const settings = { method: method.toUpperCase(), credentials: 'same-origin' };
-      options = Object.assign(settings, options);
-      const jsonHeaders = { 'Content-Type': 'application/json', 'Accept': 'application/json' };
-      options.headers = Object.assign(jsonHeaders, options.headers);
-      const toPair = (key) => key + '=' + encodeURIComponent(data[key]);
-      if (options.method === 'GET' && data)
-         url = url + (url.includes('?') ? '&' : '?') + Object.keys(data).map(toPair).join('&');
-      else if (options.method !== 'GET' && data)
-         options.body = JSON.stringify(data);
-      const logUrl = url.replace(/[?].*/, '');
-      const logDomain = logUrl.replace(/.*:[/][/]/, '').replace(/[:/].*/, '');
+      Object.assign(settings, options);
+      const isGetRequest = settings.method === 'GET';
+      const jsonHeaders = { 'Accept': 'application/json' };
+      if (!isGetRequest && data)
+         jsonHeaders['Content-Type'] = 'application/json';
+      settings.headers = Object.assign(jsonHeaders, options && options.headers);
+      const toPair = (key) => key + '=' + encodeURIComponent(data[key]);  //build query string field-value
+      const paramKeys = isGetRequest && data && Object.keys(data);
+      if (paramKeys)
+         url = url + (url.includes('?') ? '&' : '?') + paramKeys.map(toPair).join('&');
+      if (!isGetRequest && data)
+         settings.body = JSON.stringify(data);
+      const logUrl = url.replace(/[?].*/, '');  //security: prevent logging url parameters
+      const logDomain = logUrl.replace(/.*:[/][/]/, '').replace(/[:/].*/, '');  //extract hostname
       const toJson = (response) => {
          const contentType = response.headers.get('content-type');
          const isJson = /json|javascript/.test(contentType);  //match "application/json" or "text/javascript"
-         const textToObj = (httpBody) => {
+         const textToObj = (httpBody) => {  //rest calls should only return json
             response.error =       !response.ok;
             response.contentType = contentType;
             response.bodyText =    httpBody;
             return response;
             };
          if (fetchJson.logger)
-            fetchJson.logger(new Date().toISOString(), 'response', options.method,
+            fetchJson.logger(new Date().toISOString(), 'response', settings.method,
                logDomain, logUrl, response.ok, response.status, response.statusText, contentType);
          return isJson ? response.json() : response.text().then(textToObj);
          };
       if (fetchJson.logger)
-         fetchJson.logger(new Date().toISOString(), 'request', options.method, logDomain, logUrl);
-      return fetch(url, options).then(toJson);
+         fetchJson.logger(new Date().toISOString(), 'request', settings.method, logDomain, logUrl);
+      return fetch(url, settings).then(toJson);
       },
-   get:    (url, params, options) =>   fetchJson.request('GET',    url, params,   options),
+   get:    (url, params,   options) => fetchJson.request('GET',    url, params,   options),
    post:   (url, resource, options) => fetchJson.request('POST',   url, resource, options),
    put:    (url, resource, options) => fetchJson.request('PUT',    url, resource, options),
    patch:  (url, resource, options) => fetchJson.request('PATCH',  url, resource, options),
    delete: (url, resource, options) => fetchJson.request('DELETE', url, resource, options),
-   logger: null,
+   logger: null,  //null or a function
    getLogHeaders: () =>
       ['Timestamp', 'HTTP', 'Method', 'Domain', 'URL', 'Ok', 'Status', 'Text', 'Type'],
    getLogHeaderIndex: () =>
