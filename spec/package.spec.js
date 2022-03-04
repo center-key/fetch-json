4c7ce82031b00731b9e2925beb410f5519d1178d
dpilafian <dem@centerkey.com>
2022-03-04 02:00:56 -0800

    Verify dist folder has correct files

---
+++ spec/package.spec.js
@@ -0,0 +1,22 @@
+// Mocha Specification Suite
+
+// Imports
+import { assertDeepStrictEqual } from 'assert-deep-strict-equal';
+import { readdirSync } from 'fs';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+describe('The "dist" folder', () => {
+
+   it('contains the correct files', () => {
+      const actual = readdirSync('dist').sort();
+      const expected = [
+         'fetch-json.d.ts',
+         'fetch-json.dev.js',
+         'fetch-json.js',
+         'fetch-json.min.js',
+         'fetch-json.umd.cjs',
+         ];
+      assertDeepStrictEqual(actual, expected);
+      });
+
+   });
